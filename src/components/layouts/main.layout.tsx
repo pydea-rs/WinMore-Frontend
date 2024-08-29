8cf322924f6db0c9ca0287bf846cf621d0243d71
Teo kamalipour <teoakamalipour@gmail.com>
2024-08-29 12:45:13 +0330

    feat: game buttons added to layout

--- src/components/layouts/main.layout.tsx
+++ src/components/layouts/main.layout.tsx
@@ -1,13 +1,16 @@
 import { BaseProps } from '@/types/global.types'
 import Image from 'next/image'
+import { Button } from '../common/button/button'
 import Copyright from '../common/footer/copyright/copyright'
 import Footer from '../common/footer/footer'
 import Header from '../common/header/header'
+import CasinoIcon from '../icons/casino/casino'
+import ConstructionTool from '../icons/constructionTool/constructionTool'
 
 const MainLayout: BaseProps = (props) => {
   const { children } = props
   return (
-    <div className="flex flex-col min-h-[100vh] px-4 2xl:container">
+    <div className="flex flex-col px-4 2xl:container">
       <Image
         src={'/assets/images/header-rectangle.svg'}
         alt="header rectangle"
@@ -33,8 +36,23 @@ const MainLayout: BaseProps = (props) => {
           // top: 0,
         }}
       />
+
       <Header />
-      <main className="flex flex-col flex-grow mt-9">{children}</main>
+      <article>
+        <aside className="fixed left-2 top-[114px] w-full z-[999999999999]">
+          <div className="px-4 2xl:container ">
+            <div className="w-fit flex flex-col gap-4">
+              <Button kind="primary" variant="info" className="w-14 h-14 hover:bg-primary/90">
+                <CasinoIcon className="w-6 h-6" />
+              </Button>
+              <Button kind="primary" variant="info" className="w-14 h-14 hover:bg-primary/90">
+                <ConstructionTool className="w-6 h-6" />
+              </Button>
+            </div>
+          </div>
+        </aside>
+        <main className="flex flex-col flex-grow mt-9">{children}</main>
+      </article>
       <Footer className="relative z-20 flex-grow-0">
         <Copyright className="relative" />
       </Footer>
