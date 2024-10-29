9b05b6ccc5258fbc43492fa8e13a38c0645949f5
Arash Milani <33351970+milaniarash@users.noreply.github.com>
2024-10-29 16:10:15 +0330

    fixed header z-index

--- src/pages/user/wallet/index.tsx
+++ src/pages/user/wallet/index.tsx
@@ -6,8 +6,8 @@ import TabHeader from '@/components/common/tab/tabHeader/tabHeader'
 import TabItem from '@/components/common/tab/tabItem/tabItem'
 import Head from 'next/head'
 import { Fragment } from 'react'
-import ChainTokenList from './chainTokenList/chainTokenList'
-import WalletHistory from './walletHistory/walletHistory'
+import ChainTokenList from '../../../components/pages/wallet/chainTokenList/chainTokenList'
+import WalletHistory from '../../../components/pages/wallet/walletHistory/walletHistory'
 
 const Wallet = () => {
   return (
@@ -18,6 +18,12 @@ const Wallet = () => {
       <div className="flex flex-col flex-grow">
         <section>
           <Container className="z-10 overflow-x-visible">
+            <div className="md:absolute left-4 top-0 ">
+              <div className="flex items-center gap-x-2 pr-4 py-8 md:p-4 md:pl-0">
+                <span className="font-bold text-lg">Wallet</span>
+              </div>
+            </div>
+
             <Tab className="mb-8">
               <TabHeader>
                 <TabItem>Deposit/Withdraw</TabItem>
