8f90db6235e2b6d5c6d1e08d50c0c030319d91e5
Teo kamalipour <teoakamalipour@gmail.com>
2024-11-02 15:59:36 +0330

    fix: removed polygon network

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,41 +1,41 @@
 import { INetwork } from '@/types/global.types'
 
 export const networks: INetwork[] = [
-  {
-    chainId: 137, // Polygon Mainnet Chain ID
-    name: 'Polygon Mainnet',
-    rpcUrl: 'https://rpc-mainnet.maticvigil.com', // Mainnet RPC URL
-    icon: '/assets/images/chains/Polygon Mainnet.png',
-    tokens: [
-      {
-        id: 0,
-        symbol: 'MATIC',
-        name: 'Polygon Matic',
-        contractAddress: '0x$', // Native tokens do not have a contract address
-        icon: '/assets/images/tokens/MATIC.png',
-        isNativeToken: true,
-        balance: 0,
-      },
-      {
-        id: 1,
-        symbol: 'USDT',
-        name: 'Tether USD',
-        contractAddress: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT on Polygon mainnet
-        icon: '/assets/images/tokens/USDT.png',
-        isNativeToken: false,
-        balance: 0,
-      },
-      {
-        id: 2,
-        symbol: 'USDC',
-        name: 'USD Coin',
-        contractAddress: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359', // USDC on Polygon mainnet
-        icon: '/assets/images/tokens/USDC.png',
-        isNativeToken: false,
-        balance: 0,
-      },
-    ],
-  },
+  // {
+  //   chainId: 137, // Polygon Mainnet Chain ID
+  //   name: 'Polygon Mainnet',
+  //   rpcUrl: 'https://rpc-mainnet.maticvigil.com', // Mainnet RPC URL
+  //   icon: '/assets/images/chains/Polygon Mainnet.png',
+  //   tokens: [
+  //     {
+  //       id: 0,
+  //       symbol: 'MATIC',
+  //       name: 'Polygon Matic',
+  //       contractAddress: '0x$', // Native tokens do not have a contract address
+  //       icon: '/assets/images/tokens/MATIC.png',
+  //       isNativeToken: true,
+  //       balance: 0,
+  //     },
+  //     {
+  //       id: 1,
+  //       symbol: 'USDT',
+  //       name: 'Tether USD',
+  //       contractAddress: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT on Polygon mainnet
+  //       icon: '/assets/images/tokens/USDT.png',
+  //       isNativeToken: false,
+  //       balance: 0,
+  //     },
+  //     {
+  //       id: 2,
+  //       symbol: 'USDC',
+  //       name: 'USD Coin',
+  //       contractAddress: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359', // USDC on Polygon mainnet
+  //       icon: '/assets/images/tokens/USDC.png',
+  //       isNativeToken: false,
+  //       balance: 0,
+  //     },
+  //   ],
+  // },
   {
     chainId: 11155111, // Sepolia Testnet Chain ID
     name: 'Sepolia',
