11a938a4c6054f31c8b483ba0764b8aa5a6f96a5
Teo kamalipour <teoakamalipour@gmail.com>
2024-11-04 01:30:51 +0330

    tes

--- src/store/slices/auth/auth.slice.ts
+++ src/store/slices/auth/auth.slice.ts
@@ -23,7 +23,7 @@ export const authSlice = createSlice({
       const decodedToken: { exp: number } = jwtDecode(token)
       // Convert 'exp' to milliseconds and set cookie expiration
       const cookieExpiration = new Date(decodedToken.exp * 1000) // 'exp' is in seconds
-      console.log({ token, domain })
+      console.log('testttt', { token, domain })
       setCookie('token', token, { domain, expires: cookieExpiration })
       state.token = token
     },
